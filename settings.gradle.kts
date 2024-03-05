15b12023711746e034426e302e4f89253abe9ed7
moska <moskalenko.16.11.1996@gmail.com>
2024-03-05 14:55:37 +0300

    add empty module convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             content {
@@ -21,3 +22,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "MyScore"
 include(":app")
+include(":core:designsystem")
